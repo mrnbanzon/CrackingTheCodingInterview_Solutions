066eafb922f80ce7310df9d464f4993f2c2f0307
Michael Banzon <mrnbanzon@gmail.com>
2018-12-13 22:58:17 -0800

    Solution for Arrays and Strings 1.5.

--- Arrays_And_Strings/1.5.js
+++ Arrays_And_Strings/1.5.js
@@ -20,7 +20,24 @@ function oneWay(a, b) {
     let longest = a.length > b.length ? a : b;
     let shortest = a === longest ? b : a;
 
-    let aIndex = 0;
-    let bIndex = 0;
+    let lIndex = 0;
+    let sIndex = 0;
+
+    while (lIndex < longest.length && sIndex < shortest.length) {
+      if (longest[lIndex] !== shortest[sIndex]) {
+        lIndex += 1;
+        if (lIndex - sIndex > 1) return false;
+      }
+      lIndex += 1;
+      sIndex += 1;
+    }
+    return true;
   }
 }
+
+/* TESTING */
+console.log(oneWay('pale', 'ple')); //true
+console.log(oneWay('pales', 'pale')); //true
+console.log(oneWay('pale', 'bale')); //true
+console.log(oneWay('pale', 'bake')); //false
+console.log(oneWay('paa', 'pa')); //true
