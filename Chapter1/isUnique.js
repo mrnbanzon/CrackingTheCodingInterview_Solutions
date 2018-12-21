66ef91f98347b89bd4ccee1109e2c70a9ab7b536
Joanne Kim <joanneskim831@gmail.com>
2018-12-20 18:19:44 -0800

    add JS answers for checkPermutation and isUnique

--- Chapter1/isUnique.js
+++ Chapter1/isUnique.js
@@ -2,3 +2,19 @@
  * Implement an algorithm to determine if a string has all unique characters. What is you cannot use additional data structures?
  * 
  */
+
+const isUnique = (string) => {
+  if (string.length > 128) false;
+  let characters = {};
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+    if (characters[char]) {
+      return false;
+    }
+    characters[char] = true;
+  }
+  return true;
+}
+
+let string = 'helo';
+console.log(isUnique(string));
